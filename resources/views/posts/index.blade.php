51929fe6ff08abbd9a5f755106a0eff1970ab76c
Kristianti Rahayu <93201297+KRahayu@users.noreply.github.com>
2024-03-11 09:56:02 +0700

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Posts - KristiantiRahayu.my.id</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body style="background: lightgray">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 shadow rounded">
+                    <div class="card-body">
+                        <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-3">ADD POST</a>
+                        <center><h2>The Highest Mountain From Every Country In This World</h2><h2>By Kristianti Rahayu</h2></center>
+                        <br>
+                        <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <th scope="col">IMAGE</th>
+                                <th scope="col">TITLE</th>
+                                <th scope="col">CONTENT</th>
+                                <th scope="col">SOURCE</th>
+                                <th scope="col">ACTION</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              @forelse ($posts as $post)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ Storage::url('public/posts/').$post->image }}" class="rounded" style="width: 150px">
+                                    </td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{!! $post->content !!}</td>
+                                    <td>{!! $post->source !!}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Are you sure?');" action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">DELETE</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                              @empty
+                                  <div class="alert alert-danger">
+                                         Post data not yet available.
+                                  </div>
+                              @endforelse
+                            </tbody>
+                          </table>  
+                          {{ $posts->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+        //message with toastr
+        @if(session()->has('success'))
+        
+            toastr.success('{{ session('success') }}', 'Success!'); 
+
+        @elseif(session()->has('error'))
+
+            toastr.error('{{ session('error') }}', 'Failed!'); 
+            
+        @endif
+    </script>
+
+</body>
+</html>
